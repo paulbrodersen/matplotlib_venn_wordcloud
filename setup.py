b4c76c0e81f84a582e5e20613646ee8409ce0736
Paul Brodersen <paulbrodersen@gmail.com>
2017-04-04 12:33:47 +0100

    Fix setup in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
     name = 'matplotlib_venn_wordcloud',
-    packages = ['matplotlib_venn_wordcloud'],
     version = '0.1',
     description = 'Create a Venn diagram with word clouds corresponding to each subset.',
     author = 'Paul Brodersen',
@@ -12,12 +11,13 @@ setup(
     classifiers = [ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Visualization'
     ],
     platforms=['Platform Independent'],
+    packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'matplotlib-venn', 'wordcloud'],
 )
